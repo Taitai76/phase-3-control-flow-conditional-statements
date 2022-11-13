b65bf4ae138ecc4fb7ae4425d9346ee1ab47b0e0
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-11-13 09:15:46 -0700

    commit tree 4d1bd81d38e4eaf26ab25c5029330c121c2c3c36

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,58 @@
 def admin_login(username, password)
-  # your code here
+  access = if username == "admin" && password == "12345"
+           "Access granted";
+           elsif username=="ADMIN" && password == "12345"
+           "Access granted";
+           else 
+            "Access denied"
+           end
+
+return access
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  temp = if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <=65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
+  return temp;
 end
 
 def fizzbuzz(num)
-  # your code here
+  answer = if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+    elsif num % 3==0
+    "Fizz"
+    elsif num% 5==0
+      "Buzz"
+    else
+      num
+    end
+
+  return answer
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  answer = if operation== "+"
+    num1+num2
+  elsif operation=="-"
+    num1-num2
+  elsif operation == "*"
+    num1 * num2
+  elsif operation =="/"
+    num1 / num2
+  else
+    nil
+  end
+  if answer == nil
+    puts "Invalid operation!";
+  end
+
+return answer;
 end
 
